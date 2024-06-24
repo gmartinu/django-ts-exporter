fa41321ba9cc2e0a701599803c234f35b5900742
Gabriel Martinusso <gabrielbramos@outlook.com>
2024-06-24 22:42:50 +0000

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-ts-exporter",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["Django>=3.0", "djangorestframework>=3.11"],
